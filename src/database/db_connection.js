98f037a3de5adb30624812a0fe2e0a94f500f05f
Angham116 <angham.1161995@hotmail.com>
2019-02-13 09:58:05 +0200

    Made Some Editing on database connection file

--- src/database/db_connection.js
+++ src/database/db_connection.js
@@ -5,15 +5,15 @@ require('dotenv').config();
 const { Pool } = pg;
 const params = url.parse(process.env.DATABASE_URL);
 
-const options = new Pool({
-  username: params.auth.split(':')[0],
+const options = {
+  user: params.auth.split(':')[0],
   password: params.auth.split(':')[1],
   port: params.port,
   host: params.hostname,
   database: params.pathname.split('/')[1],
   ssl: params.hostname !== 'localhost',
   max: process.env.DB_MAX_CONNECTIONS || 2,
-});
+};
 
-module.exports = options;
+module.exports =new Pool(options);;
 
