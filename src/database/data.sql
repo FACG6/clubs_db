e30f79168716ccb4a91146e30107c44db3451ad9
denaSH <denamoean17@gmail.com>
2019-02-13 10:15:55 +0200

    make add club file

--- src/database/data.sql
+++ src/database/data.sql
@@ -12,9 +12,11 @@ CREATE TABLE clubs(
     id SERIAL PRIMARY KEY,
     c_name VARCHAR(20) NOT NULL,
      c_desc text,
-     type_id REFERENCES clubs_type(id) 
+     type_id integer REFERENCES clubs_type(id) 
 
 );
 
+insert into clubs_type(type_name,type_image)values('English','fhfhfj');
+insert into clubs(c_name,c_desc,type_id)values('Englishhhhh','this ishuisinhdg',1);
 COMMIT;
 
---
+++ src/database/db_connection.js
@@ -0,0 +1,17 @@
+const pg = require('pg');
+const url = require('url');
+require('dotenv').config();
+
+const { Pool } = pg;
+const params = url.parse(process.env.DATABASE_URL);
+
+const options = new Pool({
+  user: params.auth.split(':')[0],
+  password: params.auth.split(':')[1],
+  port: params.port,
+  host: params.hostname,
+  database: params.pathname.split('/')[1],
+  ssl: params.hostname !== 'localhost',
+  max: process.env.DB_MAX_CONNECTIONS || 2,
+});
+module.exports = options;
